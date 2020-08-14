4cd311fc1e154698929979da65d6378be80481b6
张斌 <522864514@qq.com>
2020-08-14 16:11:55 +0800

    马化腾来了提交代码

--- MaYun/src/haha.java
+++ MaYun/src/haha.java
@@ -1,11 +1,5 @@
 
 public interface haha {
-public void hh();
-//李康新添加一个方法
-public void likaang();
-//李康有提交一个方法
-public void likaang1();
-//马云提交代码啦
-public void mayun();
-
+//马化腾提交一个方法
+	public void mahuateng();
 }
